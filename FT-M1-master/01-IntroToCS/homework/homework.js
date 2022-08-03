4ac04d2d7416f5f08954a9972bed882d985469a9
diegoanmonsa <diego_m_saenz@hotmail.com>
2022-08-02 20:01:38 -0500

    Homework 01

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,13 +1,27 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+  const i=10;
+  var helper=num, residue=0, count=1, result=0;
+  while (helper>0){
+    residue=helper%i;
+    helper=Math.trunc(helper/i);
+    result+=residue*count;
+    count*=2;
+  }
+  return result;  
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  const i=2;
+  var helper=num, residue=0, count=1, result=0;
+  while(helper>0){
+    residue=helper%i;
+    helper=Math.trunc(helper/i);
+    result+=residue*count;
+    count*=10;
+  }
+  return result.toString();
 }
 
 
